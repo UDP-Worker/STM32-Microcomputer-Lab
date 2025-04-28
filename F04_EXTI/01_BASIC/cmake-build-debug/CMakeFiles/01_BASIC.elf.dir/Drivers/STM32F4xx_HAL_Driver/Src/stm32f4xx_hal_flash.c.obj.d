a1f723cee1b95a2ef79c83bf4a48edcfd7a7ebb0
UDP-Worker <galath.lm@gmail.com>
2025-04-28 20:22:14 +0800

    feat(EXTI-BASIC): 新建01_BASIC子实验工程

---
+++ F04_EXTI/01_BASIC/cmake-build-debug/CMakeFiles/01_BASIC.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/01_BASIC.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj: \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_flash.c \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Core\Inc/stm32f4xx_hal_conf.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f446xx.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Include/core_cm4.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\lib\gcc\arm-none-eabi\9.2.1\include\stdint.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\arm-none-eabi\include\stdint.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\arm-none-eabi\include\machine\_default_types.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\arm-none-eabi\include\sys\features.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\arm-none-eabi\include\_newlib_version.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\arm-none-eabi\include\sys\_intsup.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\arm-none-eabi\include\sys\_stdint.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Include/mpu_armv7.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ c:\gcc-arm-none-eabi\gcc-arm-none-eabi-9-2019-q4-major-win32\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_exti.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ D:\SynologyDrive\STM32\F04_EXTI\01_BASIC\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h
---
+++ F04_EXTI/01_BASIC/cmake-build-debug/CMakeFiles/01_BASIC.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for 01_BASIC.elf.
+# This may be replaced when dependencies are built.
